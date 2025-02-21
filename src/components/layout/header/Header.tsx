e3d17e5c9e56418824365ff8af937b1be335eec6
Koderkup <koderkup@gmail.com>
2025-02-21 20:19:16 +0300

    side menu header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,11 +3,21 @@ import styles from "./style.module.css";
 import logOut from "../../../icons/logOut.svg";
 import logIn from "../../../icons/logIn.svg";
 import user from "../../../icons/user.svg";
+import menu from "../../../icons/menu.svg";
+import close from "../../../icons/x.svg";
 import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
+import useMediaQuery from "../../../hooks/useMediaQurey";
 
 const Header = () => {
   const [auth, setAuth] = useState<boolean>(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const isMobile = useMediaQuery(1000);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className={cn("navbar navbar-expand-lg", styles["nav-header"])}>
       <div className="container-fluid">
@@ -15,48 +25,103 @@ const Header = () => {
           JobApplicator
         </Link>
 
-        <div className={cn("navbar-nav", styles[""])}>
+        {isMobile ? (
+          <div className={styles["menu-icon"]} onClick={toggleMenu}>
+            <img src={menu} alt="Menu Icon" />
+          </div>
+        ) : (
+          <div className={cn("navbar-nav", styles[""])}>
+            <NavLink
+              style={{ textAlign: "center" }}
+              className={({ isActive }) =>
+                cn("navbar-brand", { [styles["active"]]: isActive })
+              }
+              to="/filters"
+            >
+              ФИЛЬТРЫ
+            </NavLink>
+            <NavLink
+              style={{ textAlign: "center" }}
+              className={({ isActive }) =>
+                cn("navbar-brand", { [styles["active"]]: isActive })
+              }
+              to="/offers"
+            >
+              ПРЕДЛОЖЕНИЯ
+            </NavLink>
+            <NavLink
+              style={{ textAlign: "center" }}
+              className={({ isActive }) =>
+                cn("navbar-brand", { [styles["active"]]: isActive })
+              }
+              to="/result"
+            >
+              РЕЗУЛЬТАТ
+            </NavLink>
+          </div>
+        )}
+
+        {!isMobile && (
+          <div className={cn("navbar-nav", styles["nav-icon-wrapper"])}>
+            <div className={styles["avatar-wrapper"]}>
+              <img src={user} />
+            </div>
+            {auth ? (
+              <img src={logOut} />
+            ) : (
+              <Link to="/register">
+                <img src={logIn} />
+              </Link>
+            )}
+          </div>
+        )}
+      </div>
+
+      {menuOpen && isMobile && (
+        <div className={styles["sidebar-menu"]}>
+          <div className={styles["close-icon"]} onClick={toggleMenu}>
+            <img src={close} alt="Close Menu" />
+          </div>
           <NavLink
-            style={{ textAlign: "center" }}
             className={({ isActive }) =>
               cn("navbar-brand", { [styles["active"]]: isActive })
             }
             to="/filters"
+            onClick={toggleMenu}
           >
             ФИЛЬТРЫ
           </NavLink>
           <NavLink
-            style={{ textAlign: "center" }}
             className={({ isActive }) =>
               cn("navbar-brand", { [styles["active"]]: isActive })
             }
             to="/offers"
+            onClick={toggleMenu}
           >
             ПРЕДЛОЖЕНИЯ
           </NavLink>
           <NavLink
-            style={{ textAlign: "center" }}
             className={({ isActive }) =>
               cn("navbar-brand", { [styles["active"]]: isActive })
             }
             to="/result"
+            onClick={toggleMenu}
           >
             РЕЗУЛЬТАТ
           </NavLink>
-        </div>
-        <div className={cn("navbar-nav", styles["nav-icon-wrapper"])}>
-          <div className={styles["avatar-wrapper"]}>
+          <div className={styles["icon-wrapper-mobile"]}>
             <img src={user} />
+
+            {auth ? (
+              <img src={logOut} />
+            ) : (
+              <Link to="/register" style={{ display: "inline" }}>
+                <img src={logIn} />
+              </Link>
+            )}
           </div>
-          {auth ? (
-            <img src={logOut} />
-          ) : (
-            <Link to="/register">
-              <img src={logIn} />{" "}
-            </Link>
-          )}
         </div>
-      </div>
+      )}
     </nav>
   );
 };
