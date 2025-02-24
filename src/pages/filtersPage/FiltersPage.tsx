c0fc73676aef4b63da50a0db83a70ee68f6119ef
Koderkup <koderkup@gmail.com>
2025-02-24 14:38:17 +0300

    add plate, input textarea

--- src/pages/filtersPage/FiltersPage.tsx
+++ src/pages/filtersPage/FiltersPage.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Plate from "../../components/ui/Plate/Plate";
+import TextArea from "../../components/ui/TextArea/TextArea";
+import TextInput from "../../components/ui/TextInput/TextInput";
 
 const FiltersPage = () => {
   return (
-    <div>FiltersPage</div>
-  )
-}
+    <>
+      <div style={{ margin: "10px" }}>
+        <Plate text={"Title"} />
+        <Plate text={"Senior"} />
+        <Plate text={"Part time"} />
+      </div>
+      <div style={{ margin: "10px" }}>
+        <TextArea />
+      </div>
+      <div style={{ margin: "10px" }}>
+        <TextInput />
+      </div>
+    </>
+  );
+};
 
-export default FiltersPage
+export default FiltersPage;
